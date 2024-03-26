a63017ef5eec95e53786da9723904f1828c51af8
Fadil9876 <syriffadillahfaturahman@gmail.com>
2024-03-26 10:32:16 +0700

    12

---
+++ application/controllers/tugas1.php
@@ -0,0 +1,10 @@
+<?php
+class tugas1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1> Introduce </h1>";
+ echo"Nama saya Syarif Fadillah Faturahman 
+ Asal dari Pontianak Hobi Memancing";
+ }
+}
