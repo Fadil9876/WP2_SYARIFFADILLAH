9ba7da757e1113e84b8cc6119a7ec466a5e32be0
Fadil9876 <syriffadillahfaturahman@gmail.com>
2024-04-22 16:14:10 +0700

    pertemuan 4& tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,31 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li >Nama Jurnalis : Syarif Fadillah Faturahman</li>
+        <li>Umur           : 40</li>
+        <li>Jenis Kelamin  : Pria</li>
+        <li>Mahasiswa</li>
+       
+    </ul>
+    <h4>Riwayat Sekolah</h4>
+    <li>SD 37 PONTIANAK TENGGARA</li>
+    <li>SMP NEGERI 6 PONTIANAK SELATAN</li>
+    <li>SMA KEMALA BHAYANGKARI </li>
+    <li>UNIVERSITAS BINA SARANA INFORMATIKA</li>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Adisucipto GG Belitung 1 No.1</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Lari</li>
+    </ul>
+</section>
